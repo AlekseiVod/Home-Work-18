df3416bd6928675453ed44977fc41807d9eec369
AlekseiVod <vodianitskii.aleksei@mail.ru>
2024-07-30 14:10:11 +1000

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+# Правильный вызов
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+    inner_function()
+
+
+test_function()
+
+
+# Вызов функции inner_function вне функции test_function
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+
+
+inner_function()
+test_function()
